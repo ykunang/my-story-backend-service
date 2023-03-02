e249821d745451f9a052779e55da437b22b87745
mahmud <mahmud120398@gmail.com>
2023-03-03 06:45:59 +0700

    feat: add story factory

---
+++ database/factories/StoryFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\odel=Story>
+ */
+class StoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory()->create()->id,
+            'category_id' => random_int(1, 4),
+            'title' => fake()->title(),
+            'description' => fake()->text(100),
+            'photo' => fake()->image(dir: public_path('stories'), width:250, height:250, fullPath:false),
+        ];
+    }
+}

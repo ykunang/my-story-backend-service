1b6395e4afaf0b3466467dc7488277c9d3c1e008
mahmud <mahmud120398@gmail.com>
2023-03-03 18:15:05 +0700

    refactor: code clean up

--- app/Http/Controllers/Api/AuthServiceController.php
+++ app/Http/Controllers/Api/AuthServiceController.php
@@ -6,28 +6,28 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Http\Services\AuthService;
-use Illuminate\Http\Request;
 
 
 class AuthServiceController extends Controller
 {
-    function __construct(private AuthService $auth) {}
+    public function __construct(private AuthService $auth)
+    {}
 
-    function login(LoginRequest $request) {
+    public function login(LoginRequest $request)
+    {
         $result = $this->auth->login($request->safe()->all());
-        return $this->responseSuccess(
-            data:['token' => $result],
-            message: 'Login successfull'
-        );
+        return $this->responseSuccess(data:['token' => $result], message: 'Login successfull');
     }
 
 
-    function register(RegisterRequest $request) {
+    public function register(RegisterRequest $request)
+    {
         $user = $this->auth->register($request->safe()->all());
         return $this->responseSuccess(message: 'Register successfull', data: $user);
     }
 
-    function getProfile() {
+    public function getProfile()
+    {
         $result = $this->auth->profile();
         return $this->responseSuccess(data: $result);
     }
